5ffc4c909435d0be31131bfbe41eb6dc4179f428
e-candeloro <candeloroettore@gmail.com>
2024-12-19 15:07:49 +0100

    added model factory and example models

---
+++ source/models/ModelFactory.py
@@ -0,0 +1,48 @@
+import torch
+
+import models
+
+
+class ModelFactory:
+    @staticmethod
+    def get_model(config, args=None):
+        fallback_model = ""  # fallback model name if not specified in config
+        name = (
+            config["model"].get("name", fallback_model).lower()
+        )  # get model name from config, if not found use fallback_model, convert to lowercase
+        model_class = None
+
+        # List of custom model names excluding internal attributes
+        custom_model_names = [
+            model for model in models.__dict__ if not model.startswith("__")
+        ]
+
+        # Check for partial matches in the custom models
+        for key in custom_model_names:
+            if name in key.lower() or key.lower() in name:
+                model_class = getattr(models, key)
+                break
+
+        if model_class is None:
+            # Check for partial matches in the torch.nn.modules models if no match found in custom models
+            torch_model_names = [
+                model
+                for model in torch.nn.modules.__dict__
+                if not model.startswith("__")
+            ]
+            for key in torch_model_names:
+                if name in key.lower() or key.lower() in name:
+                    model_class = getattr(torch.nn.modules, key)
+                    break
+
+        if model_class:
+            try:
+                model_instance = model_class(args, config)
+            except TypeError as e:
+                raise TypeError(
+                    f"Could not instantiate {model_class} with {config} and {args}: {e}"
+                )
+        else:
+            raise Exception(f"Model {name} not implemented")
+
+        return model_instance
