74af9f9ef55019d082b1b39cd4a920f377b2a1ca
e-candeloro <candeloroettore@gmail.com>
2024-12-19 16:50:35 +0100

    added graphs and continued developing data ingestion pipeline

--- source/models/Detectron2.py
+++ source/models/Detectron2.py
@@ -11,13 +11,13 @@ class Detectron2(nn.Module):
         self.model_conf = self.config.get("model", None)
         assert self.model_conf is not None, "Model configuration not found!"
 
-        #other initializations, like the model
-        
+        # other initializations, like the model
+
         # self.model = ...
-            
+
     # def predict(self, data):
     #    pass
-    
+
     # def forward(self, x):
     #     return self.model(x)
 
